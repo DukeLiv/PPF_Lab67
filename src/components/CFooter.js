e5852a6381a38d60d2a514701d41a9c60deb77fc
DukeLiv <92340142+DukeLiv@users.noreply.github.com>
2022-01-21 21:06:07 +0100

    Wykład

---
+++ src/components/CFooter.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './CFooter.css';
+
+// function CFooter() {
+//     let date = new Date();
+//     return (
+//     <div>
+//         <h5>
+//             PPFront-end, przykładowy serwis React. Dzisiaj mamy:{ " " }
+//             { date.toDateString() }{ " " }
+//         </h5>
+//     </div>
+//     );
+// };
+
+export const CFooter = (props) => {
+    const { weather, temp, children } = props;
+    let date = new Date();
+    return (
+        <div>
+            <h5>
+                PPFront-end, przykładowy serwis React. Dzisiaj mamy: { date.toDateString() }
+                <br/>
+                Stan pagoda: { weather }, temperatura: { temp }.
+                <br/>
+                { children }
+            </h5>
+        </div>
+    );
+};
+
+// export const CFooter = () => {
+//     let date = new Date();
+//     return React.createElement('div', { className: 'footerDivClass' }, 
+//     React.createElement('h5', { id: 'footerText', className: 'footerTextClass' }, 
+//     'PPFront-end, przykładowy serwis React. Dzisiaj mamy: ' + date.toDateString()));
+// };
+
+// export default CFooter;
